399cd27ccd1b64f54d6f02c7074d0715bd031d4e
Le Malgache <hyperpikaevo@gmail.com>
2020-11-16 20:20:01 +0100

    Added favicon and initial base

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Contrat;
+use App\Entity\Offres;
 
 class AppController extends AbstractController
 {
@@ -13,8 +15,13 @@ class AppController extends AbstractController
      */
     public function index(): Response
     {
+        $repo = $this->getDoctrine()->getRepository(Offres::class);
+
+        $offres = $repo->findAll();
+
         return $this->render('app/index.html.twig', [
             'controller_name' => 'AppController',
+            'offres' => $offres
         ]);
     }
 }
